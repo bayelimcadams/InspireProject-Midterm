caf7e2c671b9c1ab38e07f4b90e498d1a3728467
bayelimcadams <mcadamsbay@gmail.com>
2020-04-07 05:03:23 -0600

    .

--- app/controllers/image-controller.js
+++ app/controllers/image-controller.js
@@ -1,24 +1,24 @@
-import ImageService from "../services/image-service.js";
-import store from "../store.js";
+// import ImageService from "../services/image-service.js";
+// import store from "../store.js";
 
-//TODO Create methods for constructor, and rendering the image to the page
-//      (you may wish to set it as a background image)
+// //TODO Create methods for constructor, and rendering the image to the page
+// //      (you may wish to set it as a background image)
 
-function drawImage() {
+// function drawImage() {
 
-  document.getElementById('image-area').innerHTML = store.State.image.ImageTemplate;
-  console.log("THE IMAGE MAN SAYS:", store.State.image);
-  // event.preventDefault();
+//   document.getElementById('image-area').innerHTML = store.State.image.ImageTemplate;
+//   console.log("THE IMAGE MAN SAYS:", store.State.image);
+//   // event.preventDefault();
 
-  // let template = ""
-  // let image = ImageService.getImage();
-  // document.querySelector("#sandBox-image").innerHTML = template += `<img src="${image}" class="image">`
-}
+//   // let template = ""
+//   // let image = ImageService.getImage();
+//   // document.querySelector("#sandBox-image").innerHTML = template += `<img src="${image}" class="image">`
+// }
 
-export default class ImageController {
-  constructor() {
-    drawImage();
-    store.subscribe("image", drawImage);
-    ImageService.getImage();
-  }
-}
+// export default class ImageController {
+//   constructor() {
+//     drawImage();
+//     store.subscribe("image", drawImage);
+//     ImageService.getImage();
+//   }
+// }
--- app/services/image-service.js
+++ app/services/image-service.js
@@ -1,38 +1,38 @@
-import ImageModel from "../models/image-model.js";
-import store from "../store.js";
+// import ImageModel from "../models/image-model.js";
+// import store from "../store.js";
 
-// @ts-ignore
-const imgApi = axios.create({
-  baseURL: "//bcw-sandbox.herokuapp.com/api/images",
-  timeout: 8000
-});
+// // @ts-ignore
+// const imgApi = axios.create({
+//   baseURL: "//bcw-sandbox.herokuapp.com/api/images",
+//   timeout: 8000
+// });
 
-// //TODO create methods to retrieve data trigger the update window when it is complete
-class ImageService {
+// // //TODO create methods to retrieve data trigger the update window when it is complete
+// class ImageService {
 
-  // constructor() {
-  //   this.getImage();
-  // }
+//   // constructor() {
+//   //   this.getImage();
+//   // }
 
-  // async 
-  getImage() {
-    console.log("Calling the Imageman");
+//   // async 
+//   getImage() {
+//     console.log("Calling the Imageman");
 
-    // return await 
+//     // return await 
     
-    imgApi.get()
-      .then(res => {
-      console.log(res.data);
-      let rawDataObject = res.data
-      let image = new ImageModel(rawDataObject)
-      store.commit('image', image)
+//     imgApi.get()
+//       .then(res => {
+//       console.log(res.data);
+//       let rawDataObject = res.data
+//       let image = new ImageModel(rawDataObject)
+//       store.commit('image', image)
 
-  //     store.commit("photo", res.data);
-  //     console.log("store photo", store.State.photo);
-    })
-    .catch(err => console.error(err))
-  }
-}
+//   //     store.commit("photo", res.data);
+//   //     console.log("store photo", store.State.photo);
+//     })
+//     .catch(err => console.error(err))
+//   }
+// }
 
-const imageService = new ImageService();
-export default imageService;
+// const imageService = new ImageService();
+// export default imageService;
